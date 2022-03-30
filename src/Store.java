6d878b061f09e1099b4cfe90242818a26ae29c92
Mauricio Erick Soria Cisneros <100866034+Rickmau@users.noreply.github.com>
2022-03-30 12:01:52 -0600

    Advance 30/03/2022

---
+++ src/Store.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Store {
+    //Attributes
+    //Object type ---------String
+    //Available objects ---ArrayList<Objects>
+
+    private String name;
+    private ArrayList<Object> availableObjects;
+
+
+    //Method
+    public boolean userBuy(double money,int quantity, int objectIndex){
+        //Approve that are sufficient objects of the required type
+        //We have the quantity and object
+            //If we have money, approve that is sufficient
+                // If it is sufficient, we sell
+            //If else
+                //Show that the user doesn't have the sufficient money
+            //If not
+                //We make an exception that we don´t the quantity requested
+                //Repeat the question
+        return false;
+    }
+    //Buy
+    //Sell
+    public boolean userSell(Object object, int quantity){
+        //Inverse logic of buy
+        //Infinite money on the store
+        //Receive object and give money to the user
+        return false;
+    }
+}
