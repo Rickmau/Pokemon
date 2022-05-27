31d0c1e0fabb13ded2b044180ac7d00f2205a77d
Mauricio Erick Soria Cisneros <100866034+Rickmau@users.noreply.github.com>
2022-05-27 12:11:27 -0500

    Implemented interface in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
 import java.util.ArrayList;
 
-public class Main {
+public class Main implements Interface{
     public static void main(String[] args) {
 
+    }
+
+    @Override
+    public void playedTime(long initialTime, long finalTime) {
+
+    }
+
+    @Override
+    public void showMenu() {
+
+    }
+
+    @Override
+    public Pokemon createRandomPokemon() {
+        return null;
+    }
+
+    @Override
+    public <T> T takeOutRandom(ArrayList<T> arrayList) {
+        return null;
+    }
+
+    @Override
+    public String[] weakAndStrongRandom(String type) {
+        return new String[0];
+    }
+
+    @Override
+    public String[] randomName(String type) {
+        return new String[0];
     }
 }
